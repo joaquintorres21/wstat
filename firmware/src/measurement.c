9be8d519273b54029484c9457db2fa283f26c6d9
Joaquín Torres <joaquintorresreborn@gmail.com>
2025-03-01 01:44:45 -0300

    recepcion de la señal
    faltaria comprar el sensor e ir debugeando, yo soy alto capo viste, entonces escribo todo el codigo sin probar (retraso mental severo)

--- firmware/src/measurement.c
+++ firmware/src/measurement.c
@@ -1,11 +1,15 @@
 #include "measurement.h"
 
-void IRAM_ATTR dht_check(uint8_t* state){
-    return *state = gpio_get_level(PIN_DHT);
+
+//natural power function
+int z_pow(int a, int x){
+    if(!x) return 1;
+    a = a * pow(a, x-1);
+    return a;
 }
 
-void dht_dep(int* x){
-    return;
+void IRAM_ATTR dht_check(uint8_t* state){
+    return *state = gpio_get_level(PIN_DHT);
 }
 
 float* dht_convert(dht_data raw, float* v_2d){
@@ -20,17 +24,30 @@ float* dht_convert(dht_data raw, float* v_2d){
 
 dht_data dht_get(uint8_t pin){
 
-    uint8_t st;
+    int delay;
+    uint8_t sensor_reading = 0;
+    uint8_t significance = 1;
     dht_data measurement;
-    esp_timer_handle_t t_get;
-
+    esp_timer_handle_t t_get, t_none;
+    uint16_t* current_variable[4];
+    current_variable[0] = &(measurement.z_hum);
+    current_variable[1] = &(measurement.r_hum);
+    current_variable[2] = &(measurement.z_temp);
+    current_variable[3] = &(measurement.r_temp);
+    
     const esp_timer_create_args_t get_state_timer = {
         .callback = &dht_check,
-        .arg = &st,
+        .arg = &sensor_reading,
         .name = "check0"
+    };
+    
+    const esp_timer_create_args_t none_timer = {
+        .callback = NULL,
+        .name = "default"
     };
 
     esp_timer_create(&get_state_timer, &t_get);
+    esp_timer_create(&none_timer, &t_none);
 
     //comm between esp and dht
     gpio_set_direction(PIN_DHT, GPIO_MODE_OUTPUT);
@@ -40,16 +57,23 @@ dht_data dht_get(uint8_t pin){
 
     gpio_set_direction(PIN_DHT, GPIO_MODE_INPUT); //pin is set to floating point voltage to receive data
     esp_timer_start_once(t_get, 40);
-    if(st != 0) return; //fatal error
+    if(sensor_reading != 0) return; //fatal error
     esp_timer_start_once(t_get, 80);
-    if(st != 1) return; //...
-    
+    if(sensor_reading != 1) return; //...
 
-    //a desarrollar receiving
-    dht_dep(&(measurement.z_hum));
-    dht_dep(&(measurement.r_hum));
-    dht_dep(&(measurement.z_temp));
-    dht_dep(&(measurement.r_temp));
+    uint8_t var_index = 0;
+    while(significance <= BITS_PER_BYTE){
+        esp_timer_start_once(t_none, DHT_AVERAGE);
+        **(current_variable + var_index) |= (sensor_reading << BITS_PER_BYTE - significance);
+        delay = sensor_reading ? DHT_REST_PERIOD + DHT_DELTA: DHT_REST_PERIOD + 2*DHT_DELTA;
+        esp_timer_start_once(t_get, delay);
+        significance++;
+        if(significance > BITS_PER_BYTE){
+            if(var_index == 3) break;
+            var_index++;
+            significance = 0;
+        }
+    }
 
     uint16_t sigma = measurement.r_hum + measurement.r_temp + measurement.z_hum + measurement.z_temp;
     if(sigma != measurement.checksum) return;
--- firmware/src/measurement.h
+++ firmware/src/measurement.h
@@ -2,9 +2,15 @@
 #include "driver/gpio.h"
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
+
 #define LOW 0
 #define HIGH 1
 
+#define BITS_PER_BYTE 8
+
+#define DHT_REST_PERIOD 50 //After each bit the sensor rests for 50 microseconds (us)
+#define DHT_AVERAGE 49 //Average between high-level pulse and low-level pulse. Used to measure the gpio level.
+#define DHT_DELTA 21 //Relative DHT_AVERAGE position
 
 #define DHT_HOST 1000 //Activation lapse 
 #define DHT22_PULSE 80 //Response sent by the sensor indicating that it's going 
@@ -18,11 +24,11 @@
 //DHT sends data in 40 bits, with an integer and a decimal part for each variable
 typedef struct {
 
-    uint8_t z_hum; //Humidity integer part
-    uint8_t r_hum; //Humidity decimal part
-    uint8_t z_temp; //Temperature integer part
-    uint8_t r_temp; //Temperature decimal part
-    uint8_t checksum; //Previous 4 bytes sum
+    uint16_t z_hum; //Humidity integer part
+    uint16_t r_hum; //Humidity decimal part
+    uint16_t z_temp; //Temperature integer part
+    uint16_t r_temp; //Temperature decimal part
+    uint16_t checksum; //Previous 4 bytes sum
 
 } dht_data;
 
