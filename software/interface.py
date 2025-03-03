7d0846fc2221281fcf988b317ff35068dac50154
Joaquín Torres <joaquintorresreborn@gmail.com>
2025-03-03 03:31:14 -0300

    no hay plata

--- software/interface.py
+++ software/interface.py
@@ -3,6 +3,7 @@ from screeninfo import get_monitors
 
 DHT22 = 0  
 MQ135 = 1
+BMP280 = 2
 
 SCR_DIM = get_monitors()[0]
 SCR_WIDTH = SCR_DIM.width
@@ -17,28 +18,43 @@ def is_sensor_on(sensor_id: int):
 def build_settings():
     dpg.add_button(label="Toggle DHT22", parent="OptWin", callback=lambda: toggle_sensor(DHT22))
     dpg.add_button(label="Toggle MQ135", parent="OptWin", callback=lambda: toggle_sensor(MQ135))
+    dpg.add_button(label="Toggle BMP280", parent="OptWin", callback= lambda: toggle_sensor(BMP280))
 
 
 def build_temp(data: list[int]):
-    dpg.add_plot(label="Temperatura", parent="DHT")
-    dpg.add_plot(label="Humedad", parent="DHT")
+    temp = dpg.add_plot(label="Temperatura",
+                parent="DHT", 
+                width=SCR_WIDTH*.2, 
+                height=SCR_HEIGHT*.25)
+    hum = dpg.add_plot(
+        label="Humedad", 
+        parent="DHT", 
+        width=SCR_WIDTH*.2, 
+        height=SCR_HEIGHT*.25)
 
 def build_co2(data: int):
-    dpg.add_plot(label="CO_2 en el aire", parent="MQ")
+    dpg.add_plot(label="CO_2 en el aire", parent="MQ",width=SCR_WIDTH*.2, height=SCR_HEIGHT*.25)
     
 def build_pressure(data: int):
     dpg.add_Plot(label="")
 
 def start():
     dpg.create_context()
-    with dpg.window(tag="MainWin",label="wstatViewer",):
+    
+    with dpg.font_registry():
+    # first argument ids the path to the .ttf or .otf file
+        title_font = dpg.add_font("C:\\Users\\Joaquin\\Documents\\GitHub\\wstat\\software\\airstrike.ttf", 20)
+        
+    with dpg.window(tag="MainWin",label="wstatViewer"):
+        title = dpg.add_text("wstat Viewer", tag="title", pos=[SCR_WIDTH*0.4, 10])
+        dpg.bind_item_font(title, title_font)
         pass
-    with dpg.group(tag="DHT", parent="MainWin", horizontal=True):
+    with dpg.group(tag="DHT", parent="MainWin", horizontal=True,pos=[10,SCR_HEIGHT*0.1]):
         pass
-    with dpg.group(tag="MQ", parent="MainWin", horizontal=True):
+    with dpg.group(tag="MQ", parent="MainWin", horizontal=True,pos=[10,SCR_HEIGHT*0.39]):
         pass
     dpg.add_window
-    with dpg.window(tag="OptWin",label="Settings", pos=[SCR_WIDTH*0.75,10], width=0.2*SCR_WIDTH, height=0.1*SCR_HEIGHT, no_move= True, no_close=True, no_resize=True):
+    with dpg.window(tag="OptWin",label="Configuración", pos=[SCR_WIDTH*0.75,SCR_HEIGHT*0.1], width=0.2*SCR_WIDTH, height=0.1*SCR_HEIGHT, no_move= True, no_close=True, no_resize=True):
         pass
     dpg.create_viewport(title='wstatViewer', clear_color=(50,50,50,0), width=SCR_WIDTH-10, height=SCR_HEIGHT-10)
     dpg.setup_dearpygui()
